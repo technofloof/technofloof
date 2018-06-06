b2a2972e7300b1d30a64000abe270334d2d649e5
technofloof <39973055+technofloof@users.noreply.github.com>
2018-06-05 23:25:57 -0400

    Create EIP20-MitchellCoin.sol

---
+++ Ethprojects/Coin/EIP20-MitchellCoin.sol
@@ -0,0 +1,78 @@
+/*
+Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+.*/
+
+
+pragma solidity ^0.4.21;
+
+import "./EIP20Interface.sol";
+
+
+contract MitchellCoin is EIP20Interface {
+
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) public allowed;
+    /*
+    NOTE:
+    The following variables are OPTIONAL vanities. One does not have to include them.
+    They allow one to customise the token contract & in no way influences the core functionality.
+    Some wallets/interfaces might not even bother to look at this information.
+    */
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show.
+    string public symbol;                 //An identifier: eg SBX
+
+    uint tokenPrice = 10**15;
+    function() payable {  //ICO basics
+        
+        uint TokenstoGive = msg.value/tokenPrice;
+        balances[msg.sender] += TokenstoGive;
+        address myaddress = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+        myaddress.transfer(msg.value);
+    }
+
+    function MitchellCoin (
+      
+    ) public {
+        totalSupply = 10**9;                            // Update total Suuply of token
+        balances[msg.sender] = totalSupply;               // Give the creator all initial tokens
+        name = "MitchellCoin";                                   // Set the name for display purposes
+        decimals = 3;                            // Amount of decimals for display purposes
+        symbol = "MTRC";                               // Set the symbol for display purposes
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+}
